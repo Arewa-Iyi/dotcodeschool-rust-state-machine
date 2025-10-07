92ad779ec720f6986e9062fda9328e52183c0058
Arewa _ <arewaiyi@gmail.com>
2025-10-07 18:09:44 -0400

    creating Pallet

--- rust-state-machine/target/debug/deps/rust_state_machine.d
+++ rust-state-machine/target/debug/deps/rust_state_machine.d
@@ -1,5 +1,6 @@
-C:\Users\arewa\OneDrive\Documents\Neural Transformers\PBAX\dotcodeschool-rust-state-machine\rust-state-machine\target\debug\deps\rust_state_machine.d: src\main.rs
+C:\Users\arewa\OneDrive\Documents\Neural Transformers\PBAX\dotcodeschool-rust-state-machine\rust-state-machine\target\debug\deps\rust_state_machine.d: src\main.rs src\balances.rs
 
-C:\Users\arewa\OneDrive\Documents\Neural Transformers\PBAX\dotcodeschool-rust-state-machine\rust-state-machine\target\debug\deps\rust_state_machine.exe: src\main.rs
+C:\Users\arewa\OneDrive\Documents\Neural Transformers\PBAX\dotcodeschool-rust-state-machine\rust-state-machine\target\debug\deps\rust_state_machine.exe: src\main.rs src\balances.rs
 
 src\main.rs:
+src\balances.rs:
