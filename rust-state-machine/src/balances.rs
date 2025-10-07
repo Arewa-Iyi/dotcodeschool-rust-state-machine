3d19a49ea3e9ed0e8bc4ec4a9036a175dce5b8da
Arewa _ <arewaiyi@gmail.com>
2025-10-07 18:17:30 -0400

    Implementing Pallet Initiation

--- rust-state-machine/src/balances.rs
+++ rust-state-machine/src/balances.rs
@@ -1,1 +1,19 @@
-pub struct Pallet{}
+use std::collections::BTreeMap;
+pub struct Pallet{
+    balances: BTreeMap<String, u128>,
+}
+impl Pallet {
+    pub fn new() -> Self {
+        Self {
+            balances: BTreeMap::new(),
+        }
+    }
+
+    // pub fn set_balance(&mut self, account: String, amount: u128) {
+    //     self.balances.insert(account, amount);
+    // }
+
+    // pub fn get_balance(&self, account: &String) -> Option<&u128> {
+    //     self.balances.get(account)
+    // }
+}
